2cdb3e3c7e737cbca5dd223fd16f01329f1fe142
Manh Tu VU <glmanhtu@gmail.com>
2022-11-27 22:07:13 +0100

    Remove Spring cloud starter kubernetes

---
+++ src/main/java/com/mtvu/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,20 @@
+package com.mtvu.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(p -> p
+                .path("/websocket/**")
+                .filters(f -> f.rewritePath("/websocket/(?<segment>.*)","/${segment}"))
+                .uri("http://websocket-server:80"))
+            .build();
+    }
+}
